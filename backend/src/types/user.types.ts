9d31974319f8c3d5f83d5479ea0bd66bddf815ad
gatundu2013 <brianwgatundu@gmail.com>
2025-05-24 19:15:34 +0300

    add user types

---
+++ backend/src/types/user.types.ts
@@ -0,0 +1,18 @@
+export enum Role {
+  ADMIN = "admin",
+  USER = "user",
+}
+
+export enum AccountStatus {
+  ACTIVE = "active",
+}
+
+export interface UserI {
+  username: string;
+  password: string;
+  phoneNumber: string;
+  accountBalance: number;
+  avatar: string;
+  accountStatus: AccountStatus;
+  role: Role;
+}
