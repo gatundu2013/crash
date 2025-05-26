567ff407dfaf2840715f4d5cb8babcead1a86c0f
gatundu2013 <brianwgatundu@gmail.com>
2025-05-26 11:11:07 +0300

    feat: add auth types

---
+++ backend/src/types/auth.types.ts
@@ -0,0 +1,26 @@
+export interface RegisterRequest {
+  username: string;
+  phoneNumber: string;
+  password: string;
+}
+
+export interface LoginRequest {
+  phoneNumber: string;
+  password: string;
+}
+
+export interface ResetPasswordRequest {
+  phoneNumber: string;
+  newPassword: string;
+  otp: string;
+}
+
+export interface RequestOtp {
+  phoneNumber: string;
+  otpPurpose: OtpPurpose;
+}
+
+export enum OtpPurpose {
+  REGISTRATION = "registration",
+  PASSWORD_RESET = "passwordReset",
+}
