8bcd7f97c269e22e5df46680dd680ba97692655a
gatundu2013 <brianwgatundu@gmail.com>
2025-05-28 20:34:56 +0300

    add custom colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,18 +18,31 @@ function App() {
 
     console.log(socket);
 
-    socket.on("t", (data) => setCurrentMultiplier(data));
+    socket.on("broadcastCurrentMultiplier", (data) =>
+      setCurrentMultiplier(data.multiplier)
+    );
 
     return () => {
-      socket.off("t"); // Remove all listeners for the "t" event
+      socket.off("broadcastCurrentMultiplier"); // Remove all listeners for the "t" event
     };
   }, [socket]); // Only re-run if socket changes
 
   return (
-    <div className="text-red-500 w-[100vw]  h-[100vh] relative">
-      <h1 className="text-2xl text-center absolute top-[50%] translate-y-[-50%] right-[50%] ">
-        {currentMultiplier.toFixed(2)}
-      </h1>
+    <div className="w-full  h-[100vh]">
+      <div className="flex">
+        <div className="w-full relative">
+          <div className="absolute top-0 left-0 right-0 bg-layer-3 h-[64px] flex items-center justify-between px-10">
+            <div>Logo</div>
+            <div>Dynamic depeding on auth</div>
+          </div>
+          <div className="mt-[64px] h-[calc(100vh-64px)]">
+            <h1>Body</h1>
+            <h2>{currentMultiplier.toFixed(2)}</h2>
+          </div>
+          <div>Footer</div>
+        </div>
+        <div className="w-[350px]">chats</div>
+      </div>
     </div>
   );
 }
