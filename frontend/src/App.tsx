b1b60023e7493dee3f65aa968cd135962f2bdbec
gatundu2013 <brianwgatundu@gmail.com>
2025-05-29 12:28:49 +0300

    Impliment signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import useSocketStore from "./stores/socketStore";
+import MainLayout from "./layouts/MainLayout";
+import { SignUpForm } from "./pages/auth/components/SignUpForm";
 
 function App() {
   const socket = useSocketStore((state) => state.socket);
@@ -28,21 +30,18 @@ function App() {
   }, [socket]); // Only re-run if socket changes
 
   return (
-    <div className="w-full  h-[100vh]">
-      <div className="flex">
-        <div className="w-full relative">
-          <div className="absolute top-0 left-0 right-0 bg-layer-3 h-[64px] flex items-center justify-between px-10">
-            <div>Logo</div>
-            <div>Dynamic depeding on auth</div>
+    <div className="w-full h-[100vh]">
+      <MainLayout
+        body={
+          <div className="w-full h-full flex justify-center">
+            <SignUpForm />
+            <div className="absolute bottom-4 right-4">
+              <h2>Current Multiplier: {currentMultiplier.toFixed(2)}</h2>
+            </div>
           </div>
-          <div className="mt-[64px] h-[calc(100vh-64px)]">
-            <h1>Body</h1>
-            <h2>{currentMultiplier.toFixed(2)}</h2>
-          </div>
-          <div>Footer</div>
-        </div>
-        <div className="w-[350px]">chats</div>
-      </div>
+        }
+        sidebarContent={<div>chats here</div>}
+      />
     </div>
   );
 }
---
+++ frontend/src/pages/auth/components/SignUpForm.tsx
@@ -0,0 +1,124 @@
+import { Form, FormField, SubmitButton } from "../../../components/forms";
+import { HiArrowNarrowLeft } from "react-icons/hi";
+import { Button } from "../../../components/ui/button";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import type { SubmitHandler } from "react-hook-form";
+import type { SignUpFormData } from "@/types/auth.types";
+import { signUpSchema } from "@/validations/auth.validations";
+import { Checkbox } from "../../../components/ui/checkbox";
+
+export function SignUpForm() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors, isSubmitting },
+    setValue,
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      phoneNumber: "",
+      username: "",
+      password: "",
+      agreeToTerms: false,
+    },
+    resolver: yupResolver(signUpSchema),
+  });
+
+  // Handle checkbox change
+  const handleCheckboxChange = (checked: boolean) => {
+    setValue("agreeToTerms", checked, { shouldValidate: true });
+  };
+
+  const onSubmit: SubmitHandler<SignUpFormData> = async (
+    data: SignUpFormData
+  ) => {
+    try {
+      // Simulating API call
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      console.log("Form submitted:", data);
+
+      // Navigate to next page after successful signup
+      console.log("Signup successful");
+    } catch (error) {
+      console.error("Signup failed:", error);
+    }
+  };
+
+  return (
+    <div className="w-full h-fit max-w-md mx-auto mt-5">
+      <div className="flex items-center mb-6">
+        <Button
+          variant="secondary"
+          className="p-0 text-white bg-transparent"
+          onClick={() => console.log("Go back")}
+        >
+          <HiArrowNarrowLeft className="h-5 w-5" />
+        </Button>
+        <h1 className="text-xl font-semibold w-full text-center">Sign Up</h1>
+      </div>
+
+      <Form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          label="Phone Number"
+          {...register("phoneNumber")}
+          type="tel"
+          placeholder="e.g 0796 549 576"
+          error={errors.phoneNumber?.message}
+          required
+        />
+
+        <FormField
+          label="Username"
+          {...register("username")}
+          placeholder="Choose a username"
+          error={errors.username?.message}
+          required
+        />
+
+        <FormField
+          label="Password"
+          {...register("password")}
+          type="password"
+          placeholder="Create a password"
+          error={errors.password?.message}
+          required
+        />
+
+        <div className="space-y-1.5">
+          <div className="flex items-center gap-2">
+            <Checkbox
+              checked={getValues().agreeToTerms}
+              onCheckedChange={handleCheckboxChange}
+            />
+            <label className="text-sm leading-tight cursor-pointer">
+              <span className="leading-tight text-sm">
+                I agree to the{" "}
+                <a href="#" className="text-green-1 hover:underline">
+                  Terms and Conditions
+                </a>{" "}
+                & confirm I am at least 18 years old
+              </span>
+            </label>
+          </div>
+          {errors.agreeToTerms?.message && (
+            <p className="text-red-500 text-sm ml-6">
+              {errors.agreeToTerms.message}
+            </p>
+          )}
+        </div>
+
+        <SubmitButton isLoading={isSubmitting} className="mt-3">
+          Sign Up
+        </SubmitButton>
+      </Form>
+
+      <div className="mt-5 text-center space-x-1 text-sm">
+        <span>Already have an account?</span>
+        <a href="#" className="text-green-1 font-medium hover:underline">
+          Sign In
+        </a>
+      </div>
+    </div>
+  );
+}
