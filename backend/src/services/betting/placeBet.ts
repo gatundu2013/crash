b23aa263e42edd39e9d2951593162f1827b36495
gatundu2013 <brianwgatundu@gmail.com>
2025-06-01 20:55:13 +0300

    add betting service

---
+++ backend/src/services/betting/placeBet.ts
@@ -0,0 +1,79 @@
+import mongoose from "mongoose";
+import User from "../../models/user.model";
+import { BetStatus, BettingPayload, SingleBet } from "../../types/bet.types";
+import BetHistory from "../../models/betHistory.model";
+import { RoundStateManager } from "../game/roundStateManager";
+import { BettingError } from "../../utils/errors/bettingError";
+
+export async function placeBet(params: BettingPayload): Promise<void> {
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const { stake, userId, autoCashoutMultiplier, clientSeed } = params;
+
+    const user = await User.findById(userId).session(session);
+
+    if (!user) {
+      throw new BettingError({
+        description: "Failed. User does not exist",
+        httpCode: 400,
+        isOperational: true,
+      });
+    }
+
+    if (stake > user.accountBalance) {
+      throw new BettingError({
+        description: "Insufficient balance",
+        httpCode: 400,
+        isOperational: true,
+      });
+    }
+
+    // create bet history
+    const newBet = await BetHistory.create(
+      [
+        {
+          roundId: RoundStateManager.getInstance().getState().roundId,
+          userId,
+          stake,
+          payout: null,
+          cashoutMultiplier: null,
+          finalMultiplier: null,
+          autoCashoutMultiplier,
+          status: BetStatus.PENDING,
+        },
+      ],
+      { session }
+    );
+
+    // Deduct the stake from user balance
+    await user.updateOne({ $inc: { accountBalance: -stake } }, { session });
+
+    await session.commitTransaction();
+
+    RoundStateManager.getInstance().addBet({
+      userId: newBet[0].userId,
+      betId: (newBet[0] as any)._id.toString(),
+      payout: newBet[0].payout,
+      status: newBet[0].status,
+      stake: newBet[0].stake,
+      autoCashoutMultiplier: newBet[0].autoCashoutMultiplier,
+      cashoutMultiplier: newBet[0].cashoutMultiplier as number | null,
+    });
+
+    RoundStateManager.getInstance().updateClientSeed({
+      seed: clientSeed,
+      userId,
+    });
+  } catch (err) {
+    await session.abortTransaction();
+
+    console.error("Failed to placeBet:", err);
+
+    throw err;
+  } finally {
+    session.endSession();
+  }
+}
