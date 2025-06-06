e16cdbdcc46776f7ac451984c38ce4d39035e1d8
gatundu2013 <brianwgatundu@gmail.com>
2025-06-06 19:16:09 +0300

    delete betting router

--- backend/src/routes/v1/betting.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { placeBetController } from "../../controllers/betting";
-
-export const betRouter = Router();
-
-betRouter.post("/placebet", placeBetController);
-
-export default betRouter;
