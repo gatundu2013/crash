c7580f74b32e20b1dd568f3aedff04f64ce230af
gatundu2013 <brianwgatundu@gmail.com>
2025-06-09 15:34:06 +0300

    refactor: move gameListeners list inside the hook

--- frontend/src/hooks/useGameListeners.ts
+++ frontend/src/hooks/useGameListeners.ts
@@ -9,49 +9,48 @@ import type {
 } from "@/types/game.types";
 import { useEffect } from "react";
 
-const {
-  handlePreparingPhase,
-  handleRunningPhase,
-  handleEndPhase,
-  handleBettingPhase,
-} = useGameStore.getState();
-
-const gameListeners = [
-  {
-    eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.PREPARING,
-    handler: (data: PreparingPhaseData) =>
-      handlePreparingPhase(data.hashedServerSeed),
-  },
-  {
-    eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.RUNNING,
-    handler: (data: RunningPhaseData) =>
-      handleRunningPhase(data.currentMultiplier),
-  },
-  {
-    eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.END,
-    handler: (data: EndPhaseData) => handleEndPhase(data.finalCrashPoint),
-  },
-  {
-    eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.BETTING,
-    handler: (data: BettingPhaseData) => handleBettingPhase(data.countDown),
-  },
-];
-
 const useGameListeners = () => {
   const socket = useSocketStore((state) => state.socket);
+  const {
+    handlePreparingPhase,
+    handleRunningPhase,
+    handleEndPhase,
+    handleBettingPhase,
+  } = useGameStore.getState();
 
   useEffect(() => {
     if (!socket) return;
 
-    //setup all listeners
+    const gameListeners = [
+      {
+        eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.PREPARING,
+        handler: (data: PreparingPhaseData) =>
+          handlePreparingPhase(data.hashedServerSeed),
+      },
+      {
+        eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.RUNNING,
+        handler: (data: RunningPhaseData) =>
+          handleRunningPhase(data.currentMultiplier),
+      },
+      {
+        eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.END,
+        handler: (data: EndPhaseData) => handleEndPhase(data.finalCrashPoint),
+      },
+      {
+        eventName: SOCKET_EVENTS.LISTENERS.GAME_PHASE.BETTING,
+        handler: (data: BettingPhaseData) => handleBettingPhase(data.countDown),
+      },
+    ];
+
+    // Setup all listeners
     gameListeners.forEach((e) => {
       socket.on(e.eventName, e.handler);
     });
 
-    // Cleanup listeners on unmount or socket change
+    // Cleanup all listeners
     return () => {
       gameListeners.forEach((e) => {
-        socket.off(e.eventName);
+        socket.off(e.eventName, e.handler);
       });
     };
   }, [socket]);
